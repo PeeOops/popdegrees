2c4ad6a808e482284146872efac5003a5a7a3b58
PeeOops <liem.richiee@gmail.com>
2024-10-12 10:53:37 +0700

    Added Responsives for Popular Movies Section

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return(
+        <div>
+            <footer className="bg-red-950 montserrat mt-24 text-white">
+                <p>Test</p>
+            </footer>
+        </div>
+    )    
+}
+
+export default Footer;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Navigation from "../components/Navigation";
+import Footer from "../components/Footer";
 import Hero from '../assets/hero.jpg';
 import Deadpool from '../assets/movies/deadpool.jpg';
 import BadBoys from '../assets/movies/bad-boys.jpg';
@@ -26,26 +27,32 @@ const Home = () => {
                 </div>
             </div>
 
-            <div className="flex justify-center items-center h-auto flex-col mt-10">
-                <p className="text-left apothem font-bold text-3xl">Popular Movies</p>
+            <div className="flex justify-center items-center h-auto flex-col mt-10 apothem font-bold">
+                <p className="text-left text-3xl">Popular Movies</p>
                 <div className="flex flex-row space-x-2 mt-2">
-                    <img src={Venom} alt="" className="w-80" />
+                    <img src={Venom} alt="" className="w-24 sm:w-48 md:w-64 lg:w-80" />
                     <div className="flex flex-col">
                         <div className="flex space-x-2">
-                            <img src={Deadpool} alt="" className="w-40" />
-                            <img src={BadBoys} alt="" className="w-40" />
-                            <img src={FamilyGuy} alt="" className="w-40" />
-                            <img src={InsideOut} alt="" className="w-40" />
+                            <img src={Deadpool} alt="" className="w-12 sm:w-24 md:w-32 lg:w-40" />
+                            <img src={BadBoys} alt="" className="w-12 sm:w-24 md:w-32 lg:w-40" />
+                            <img src={FamilyGuy} alt="" className="w-12 sm:w-24 md:w-32 lg:w-40" />
+                            <img src={InsideOut} alt="" className="w-12 sm:w-24 md:w-32 lg:w-40" />
                         </div>
                         <div className="flex space-x-2 mt-2">
-                            <img src={Joker} alt="" className="w-40" />
-                            <img src={Platform} alt="" className="w-40" />
-                            <img src={Smile} alt="" className="w-40" />
-                            <img src={Speak} alt="" className="w-40" />
+                            <img src={Joker} alt="" className="w-12 sm:w-24 md:w-32 lg:w-40" />
+                            <img src={Platform} alt="" className="w-12 sm:w-24 md:w-32 lg:w-40" />
+                            <img src={Smile} alt="" className="w-12 sm:w-24 md:w-32 lg:w-40" />
+                            <img src={Speak} alt="" className="w-12 sm:w-24 md:w-32 lg:w-40" />
                         </div>
                     </div>
                 </div>
+                <div className="flex flex-col align-center justify-center items-center mt-2">
+                    <div className="border-l-2 border-dashed border-black h-12"></div>
+                    <p className="border-dashed border-2 border-black p-4">View All</p>
+                </div>
+
             </div>
+            <Footer />
 
         </div>
     )

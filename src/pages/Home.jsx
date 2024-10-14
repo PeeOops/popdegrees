8f9ab695fc9292461379ed3006b1d729493e6e03
PeeOops <liem.richiee@gmail.com>
2024-10-14 11:52:29 +0700

    Fix Home Layout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -89,17 +89,17 @@ const Home = () => {
             <img src={Film} alt="film" className="-z-10 hidden lg:block absolute w-120 m-0 p-0" loading="lazy"/>
             <div className="flex justify-center items-center h-auto flex-col mt-10 apothem font-bold z-10">
                 <p className="text-left text-3xl">Popular Movies</p>
-                <div className="flex flex-row space-x-2 mt-2">
+                <div className="flex flex-row space-x-2 mt-2 items-center">
                     {popularMovies.slice(0,1).map((movie) => (
-                        <Link to={`movies/${movie.id}`} className="block"> <img key={movie.id} src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={movie.title} className="w-24 sm:w-48 md:w-64 lg:w-80 cursor-pointer" loading="lazy"  /></Link>
+                        <Link to={`movies/${movie.id}`} className="m-0 p-0"> <img key={movie.id} src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={movie.title} className="w-24 sm:w-48 md:w-64 lg:w-80 cursor-pointer" loading="lazy"  /></Link>
                     ))}
-                    <div className="flex flex-col">
+                    <div className="flex flex-col space-y-2">
                         <div className="flex space-x-2">
                             {popularMovies.slice(1,5).map((movie) => (
                                 <Link to={`movies/${movie.id}`} className="m-0 p-0"><img  key={movie.id} src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={movie.title} className="w-12 sm:w-24 md:w-32 lg:w-40 cursor-pointer" loading="lazy" /></Link>
                             ))}
                         </div>
-                        <div className="flex space-x-2 mt-2">
+                        <div className="flex space-x-2">
                             {popularMovies.slice(5,9).map((movie) => (
                                 <Link to={`movies/${movie.id}`} className="m-0 p-0"><img  key={movie.id} src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={movie.title} className="w-12 sm:w-24 md:w-32 lg:w-40 cursor-pointer" loading="lazy" /></Link>
                             ))}
@@ -116,15 +116,15 @@ const Home = () => {
             <img src={Film} alt="film" className="-z-10 hidden lg:block absolute right-0 w-120 m-0 p-0 transform scale-x-[-1]" loading="lazy"/>
             <div className="flex justify-center items-center h-auto flex-col mt-10 apothem font-bold z-10">
                 <p className="text-left text-3xl">Top Rated Series</p>
-                <div className="flex flex-row space-x-2 mt-2">
+                <div className="flex flex-row space-x-2 mt-2 items-center">
 
-                    <div className="flex flex-col">
+                    <div className="flex flex-col items-center space-y-2">
                         <div className="flex space-x-2">
                             {topSeries.slice(1,5).map((series) => (
                                 <img key={series.id} src={`https://image.tmdb.org/t/p/w500/${series.poster_path}`} alt={series.name} className="w-12 sm:w-24 md:w-32 lg:w-40" loading="lazy" />
                             ))}
                         </div>
-                        <div className="flex space-x-2 mt-2">
+                        <div className="flex space-x-2">
                             {topSeries.slice(5,9).map((series) => (
                                 <img key={series.id} src={`https://image.tmdb.org/t/p/w500/${series.poster_path}`} alt={series.name} className="w-12 sm:w-24 md:w-32 lg:w-40" loading="lazy" />
                             ))}
@@ -146,13 +146,13 @@ const Home = () => {
             <div className="flex justify-center items-center h-auto flex-col mt-10 apothem font-bold z-10">
                 <p className="text-left text-3xl">Upcoming Movies</p>
                 <div className="flex flex-row space-x-2 mt-2">
-                    <div className="flex flex-col">
+                    <div className="flex flex-col space-y-2">
                         <div className="flex space-x-2">
                             {upcomingMovies.slice(1,7).map((movie) => (
                                 <img  key={movie.id} src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={movie.title} className="w-12 sm:w-24 md:w-32 lg:w-40" loading="lazy" />
                             ))}
                         </div>
-                        <div className="flex space-x-2 mt-2">
+                        <div className="flex space-x-2">
                             {upcomingMovies.slice(7,13).map((movie) => (
                                 <img key={movie.id} src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={movie.title} className="w-12 sm:w-24 md:w-32 lg:w-40" loading="lazy" />
                             ))}

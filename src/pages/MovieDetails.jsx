edf5052f688f6e0ace9037e6f8b61d9ea1906b6c
PeeOops <liem.richiee@gmail.com>
2024-10-17 09:35:23 +0700

    Fixed Review Layout

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowLeft, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { faArrowLeft, faArrowRight, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 import formatDate from '../Utils';
 
 
@@ -200,7 +200,7 @@ const MovieDetails = () => {
                                 loading ?
                                 <div className="loader animate-spin border-4 border-t-4 border-gray-200 border-t-blue-500 rounded-full w-[2vw] h-[2vw] m-auto my-28"></div> :
                                 // Filter to show only Teaser and Trailer Movies
-                                medias.filter(media => media.type === "Teaser" || media.type === "Trailer").length < 0 ? (
+                                medias.filter(media => media.type === "Teaser" || media.type === "Trailer").length > 0 ? (
                                     medias.filter(media => media.type === "Teaser" || media.type === "Trailer").map((media) => ( 
                                         <iframe key={media.key} className="w-full h-[20vw] aspect-video lazyload mb-1" src={`https://www.youtube.com/embed/${media.key}`}frameborder="0" title={media.name} allowFullScreen></iframe>
                                     ))) :
@@ -214,21 +214,19 @@ const MovieDetails = () => {
 
                     {/* Reviews */}
                     <div className={isNavChange === "reviews" ? "block" : "hidden"}>
-                        <p className="mt-2 text-lg font-bold">Movie Reviews</p>
-                        
-                            
-
-                        <div className="flex flex-row space-x-2 items-center lg:space-y-8">
-                                <i class="fa-solid fa-arrow-left cursor-pointer" onClick={handleClickPreviousReview} disabled={currentReview === 0}></i>
-                                <div className="bg-white text-red-950 rounded-md p-2 lg:w-full h-full border-yellow-300 border-4     ">
-                                <p className="font-bold">{reviews[currentReview]?.author}</p>
-                                <div className="flex flex-row justify-between items-center">
-                                    <p><b>Ratings:</b> {reviews[currentReview]?.author_details.rating} / 10</p>
-                                    <p><b>Reviewed on:</b> {formatDate(reviews[currentReview]?.created_at)}</p>
+                        <p className="text-[1.2vw] font-bold my-2">Movie Reviews</p>
+                        <div className="flex flex-row items-center space-y-2">
+                                <FontAwesomeIcon icon={faArrowLeft} onClick={handleClickPreviousReview} disabled={currentReview === 0} className="cursor-pointer mx-2" />
+                                {/* NB: Check Height */}
+                                <div className="bg-red-950 text-white border-white border-2 rounded-md p-2 w-full h-64">
+                                    <p><b>Author:</b> {reviews[currentReview]?.author}</p>
+                                    <div className="flex flex-row justify-between items-center">
+                                        <p><b>Ratings:</b> {reviews[currentReview]?.author_details.rating} / 10</p>
+                                        <p><b>Date:</b> {formatDate(reviews[currentReview]?.created_at)}</p>
+                                    </div>
+                                    <p className="mt-8 line-clamp-6 font-medium text-justify">{reviews[currentReview]?.content}</p>
                                 </div>
-                                <p className="mt-8 lg:line-clamp-6 font-medium">{reviews[currentReview]?.content}</p>
-                                
-                                </div>
+                                <FontAwesomeIcon icon={faArrowRight} onClick={handleClickNextReview} disabled={currentReview === reviews.length - 1} className="cursor-pointer mx-2" />
                                 <i class="fa-solid fa-arrow-right cursor-pointer" onClick={handleClickNextReview} disabled={currentReview === reviews.length - 1}></i>
                                 </div>
 

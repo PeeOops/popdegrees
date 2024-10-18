89b80ebeaa046a51bdfcbb58f114799594249931
PeeOops <liem.richiee@gmail.com>
2024-10-18 11:05:36 +0700

    Added URL Page configuration and Next Page Button

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,21 +1,30 @@
 import { useEffect, useState } from "react";
 import Footer from "../components/Footer";
 import Navigation from "../components/Navigation";
-import { Link } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 
 const API_KEY = process.env.API_KEY;
 const BASE_URL = 'https://api.themoviedb.org/3';
 
 const Movies = () => {
 
+    const location = useLocation();
+    const navigate = useNavigate();
     const [genres, setGenres] = useState([]);
     const [movies, setMovies] = useState([]);
-    const [page, setPages] = useState(1);
+    const [page, setPage] = useState(1);
     const [error, setError] = useState("");
     const [loading, setLoading] = useState(true);
 
+    
+
     useEffect(() => {
-        const fetchData = async () => {
+
+        const queryParams = new URLSearchParams(location.search);
+        const pageFromUrl = parseInt(queryParams.get('page')) || 1;
+        setPage(pageFromUrl);
+
+        const fetchData = async (page) => {
             try {
                 const [genresURL, moviesURL] = await Promise.all([
                     fetch(`${BASE_URL}/genre/movie/list?api_key=${API_KEY}`).then((res) => {
@@ -41,8 +50,14 @@ const Movies = () => {
 
         }
 
-        fetchData();
-    })
+        fetchData(pageFromUrl);
+    },[location.search], page)
+
+    const handleNextPageButton = () => {
+        const nextPage = page + 1;
+        setPage(nextPage); 
+        navigate(`/movies?page=${nextPage}`); 
+    }
 
     return(
         <div>
@@ -76,9 +91,16 @@ const Movies = () => {
                                 <Link to={`/movies/${movie.id}`} className="m-0 p-0 hover:scale-110 transition-transform duration-300 ease-in-out inline-flex mb-2 mr-2 ml-2 w-1/6"><img  key={movie.id} src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={movie.title} className="w-12 sm:w-24 md:w-32 lg:w-40 cursor-pointer " /></Link>
                             ))
                         }
+                        
+                    </div>
+                    
+                    <div className="flex justify-between ml-6">
+                        <button className={`bg-red-950 text-white p-2 rounded-md hover:text-yellow-300 ${page === 1 ? "cursor-not-allowed" : "cursor-pointer"}`} disabled={page === 1}>Prev</button>
+                        <button className="bg-red-950 text-white p-2 rounded-md cursor-pointer hover:text-yellow-300" onClick={handleNextPageButton}>Next</button>
                     </div>
                 </div>
             </div>
+
             <Footer />
         </div>
     )
